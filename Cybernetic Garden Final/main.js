c578491bc47c2d3314c7489c62104424762c9c79
bluejaywalk <borgward@usc.edu>
2021-02-08 14:53:59 -0600

    cybernetic garden secondary commit

--- Cybernetic Garden Final/main.js
+++ Cybernetic Garden Final/main.js
@@ -1,25 +1,30 @@
 $(document).ready(function () {
 
-    
-    $('.row1').click(function() {
-      $('#plant1').slideToggle("slow");
+
+    $('.row1').click(function () {
+        $('#plant1').slideToggle("slow");
         $("#text1").fadeToggle("slow");
     });
-    
-    $('.row2').click(function() {
-      $('#plant2').slideToggle("slow");
+
+    $('.row2').click(function () {
+        $('#plant2').slideToggle("slow");
         $("#text2").fadeToggle("slow");
     });
-    
-      $('.row3').click(function() {
-      $('#plant3').slideToggle("slow");
+
+    $('.row3').click(function () {
+        $('#plant3').slideToggle("slow");
         $("#text3").fadeToggle("slow");
     });
-    
-        $('.row4').click(function() {
-      $('#plant4').slideToggle("slow");
+
+    $('.row4').click(function () {
+        $('#plant4').slideToggle("slow");
         $("#text4").fadeToggle("slow");
     });
 
-
+    $(".left").mouseenter(function () {
+        alert("What's so great about physical forms?");
+    });
+     $(".right").mouseenter(function () {
+        alert("What's so great about digital forms?");
+    });
 });
